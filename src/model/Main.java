4389c6e1d6fecfd231299306daafbe405990fd8d
João Bolsson <joaovictorbolsson@gmail.com>
2019-04-09 13:23:04 -0300

    parametros iniciais

--- src/model/Main.java
+++ src/model/Main.java
@@ -7,4 +7,33 @@ package model;
  */
 public class Main {
 
+    private static final byte M_INIT = 3, C_INIT = 3;
+    private static final State.Margin INITIAL_MARGIN = State.Margin.Left;
+
+    private static final byte M_FINAL = 3, C_FINAL = 3;
+    private static final State.Margin FINAL_MARGIN = State.Margin.Right;
+
+    private final State initialState, finalState;
+
+    private Main() {
+        // initial state
+        initialState = new State(M_INIT, C_INIT, INITIAL_MARGIN);
+        // objective
+        finalState = new State(M_FINAL, C_FINAL, FINAL_MARGIN);
+    }
+
+    private void run() {
+        // do nothing
+    }
+
+    /**
+     * Main function.
+     *
+     * @param args Command line arguments (ignore).
+     */
+    public static void main(final String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+
 }
