a21e3ed22ac5d89ae1d486c91156b9a11880c6c8
João Bolsson <joaovictorbolsson@gmail.com>
2019-05-04 20:41:32 -0300

    inicia trabalho 2

---
+++ Trab2/src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ *
+ * @author João Bolsson (jvmarques@inf.ufsm.br)
+ * @version 2019, May 04.
+ */
+public class Main {
+
+    /**
+     * Main function.
+     *
+     * @param args Command line arguments.
+     */
+    public static void main(final String[] args) {
+
+    }
+
+}
